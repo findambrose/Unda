e117db708bb711d72e714799b65f4d8527c3dfef
dES53b <58108792+dES53b@users.noreply.github.com>
2020-09-20 18:08:39 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,6 +56,7 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
+                                <a class ="btn btn-success btn-lg" href="/register" role="button">Register</a>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
